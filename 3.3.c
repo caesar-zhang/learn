afae3c890a9f188fed7783c42ac43a3a47cd9f6a
caesar-zhang <Caesar_zhang.xa@foxmail.com>
2019-12-11 23:19:32 +0800

    the third article

---
+++ 3.3.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: 3.3.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2019年12月11日 星期三 23时17分48秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+    printf("\a");
+    printf("Startled by the sudden sound,Sally shouted,\n");
+    printf("\"By the Great Pumpkin,what was that!\"");
+    return 0;
+}
+
