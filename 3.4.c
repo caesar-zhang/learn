db3762842ec95c27d8d2776ae4b0a84bed43c8c2
caesar-zhang <Caesar_zhang.xa@foxmail.com>
2019-12-12 15:35:49 +0800

    the four

---
+++ 3.4.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: 3.4.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2019年12月12日 星期四 15时33分29秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+    float  a;
+    printf("Enter a floating-point value:\n");
+    scanf("%f",&a);
+    printf("fixed-point notation:%f\n",a);
+    printf("exponential notation:%e\n",a);
+    printf("p notation:%p\n",a);
+    return 0;
+}
