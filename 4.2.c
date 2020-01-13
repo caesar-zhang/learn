6b138e4b8be7634f4b50f38be8cca7b441a29d57
caesar-zhang <Caesar_zhang.xa@foxmail.com>
2020-01-13 19:57:59 +0800

    practice

--- 4.2.c
+++ 4.2.c
@@ -6,12 +6,16 @@
  ************************************************************************/
 
 #include<stdio.h>
+#include<string.h>
 int main()
 {
     char i[100];
-    printf("请输入名字：\n");
+    int l;
+    printf("请输入名字:");
     scanf("%s",i);
-    printf("\"%s\"",i);
-
-    return 0;
+    printf("\"%s\"\n",i);//a要求
+    printf("\"%20s\"""\n",i);//b要求
+    printf("\"%-20s\"\n",i);//c要求
+    l=strlen(i);
+    printf("%*s",l+3,i);//d要求
 }
